0af2762d8b3a303384481c484fa00a5a9c5f7cc9
Matt Maginniss <maginniss.matt@gmail.com>
2016-01-29 19:43:41 -0500

    Wrote method to append a tile to the tilegroup

--- tile_group.rb
+++ tile_group.rb
@@ -1,11 +1,18 @@
 class TileGroup
 
   def initialize
-
+    @tiles = {}
   end
 
   def append(tile)
-
+    case tile
+      when Symbol
+        if @tiles.has_key?(tile)
+          @tiles[tile] = @tiles[tile] + 1;
+        else
+          @tiles[tile] = 1;
+        end
+    end
   end
 
   def remove(tile)
