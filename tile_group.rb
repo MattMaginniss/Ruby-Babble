5efed232cb56d6bac2719a5ec5ae52148fe7a201
Matt Maginniss <maginniss.matt@gmail.com>
2016-01-29 19:59:55 -0500

    Created method remove to remove a tile from the tilegroup

--- tile_group.rb
+++ tile_group.rb
@@ -13,10 +13,19 @@ class TileGroup
           @tiles[tile] = 1;
         end
     end
+    return @tiles
   end
 
   def remove(tile)
-
+    case tile
+      when Symbol
+        if @tiles.has_key?(tile)
+          @tiles[tile] = @tiles[tile] - 1;
+        else
+          @tiles[tile] = 0;
+        end
+    end
+    return @tiles
   end
 
   def to_s
