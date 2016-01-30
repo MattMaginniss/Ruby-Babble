21052a93baa735821f729394ae38f9aac1830600
Matt Maginniss <maginniss.matt@gmail.com>
2016-01-30 18:22:12 -0500

    Added body of test cases for empty group and single tile group

--- tests/when_converting_tile_group_to_string.rb
+++ tests/when_converting_tile_group_to_string.rb
@@ -5,17 +5,19 @@ class WhenConvertingTileGroupToString < Minitest::Test
 
 
   def setup
-    @tg = TileGroup.New
+    @tg = TileGroup.new
   end
 
 
   def test_convert_empty_group_to_string
+    assert_equal "", @tg.to_s
 
   end
 
 
   def test_convert_single_tile_group_to_string
-
+    @tg.append(:a)
+    assert_equal "a", @tg.to_s
   end
 
 
