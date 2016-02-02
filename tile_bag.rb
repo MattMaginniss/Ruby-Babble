73e85dd6b23f09ed729bcbb8c6acb1e522e809d6
Matt Maginniss <maginniss.matt@gmail.com>
2016-02-01 19:57:03 -0500

    Created TileBag class with stubbed out methods.

---
+++ tile_bag.rb
@@ -0,0 +1,23 @@
+# Class that represents a bag of letter tiles for Scrabble
+class TileBag
+
+  # This method initializes an object of the type TileBag
+  def initialize
+    @bag = Array.new
+  end
+
+  def draw_tile
+
+  end
+
+  def empty?
+
+  end
+
+  def self.point_for (tile)
+
+  end
+
+
+
+end
