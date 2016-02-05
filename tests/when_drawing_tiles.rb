64aa78be1ef0bd88f02b3871c6d134efdc5cfd7c
Matt Maginniss <maginniss.matt@gmail.com>
2016-02-04 23:01:28 -0500

    Stubbed out methods for both new test classes.

--- tests/when_drawing_tiles.rb
+++ tests/when_drawing_tiles.rb
@@ -1,7 +1,22 @@
 require "minitest/autorun"
 require_relative "../tile_bag.rb"
 
-# Test class for the Word's score method
-class WwhenDrawingTiles < Minitest::Test
+# Test class for the Tile Bag's DrawTile method.
+class WhenDrawingTiles < Minitest::Test
 
+  # This method sets up the test class
+  def setup
+    @tb = TileBag.new
+  end
+
+  # This tests that there can be exactly 98 tiles drawn and at the end the bag will be empty.
+  def test_has_proper_number_of_tiles
+
+  end
+
+  # This tests to see the tiles in the bag are distributed as specified by scrabble rules.
+  def test_has_proper_tile_distribution
+  
+  end
+  
 end
--- tests/when_getting_points.rb
+++ tests/when_getting_points.rb
@@ -1,8 +1,17 @@
 require "minitest/autorun"
 require_relative "../tile_bag.rb"
 
-# Test class for the Word's score method
+# Test class for the TileBag's points for method.
 class WhenGettingPoints < Minitest::Test
 
+  # This method sets up the test class
+  def setup
+    @tb = TileBag.new
+  end
+
+  # This tests if you will get the correct value for the 26 different tiles.
+  def test_confirm_point_values
+
+  end
 
 end
