008a665ff3130ce2ca165fb44000e64128673080
Matt Maginniss <maginniss.matt@gmail.com>
2016-02-04 22:55:49 -0500

    Created two test classes for the TileBag class.

---
+++ tests/when_drawing_tiles.rb
@@ -0,0 +1,7 @@
+require "minitest/autorun"
+require_relative "../tile_bag.rb"
+
+# Test class for the Word's score method
+class WwhenDrawingTiles < Minitest::Test
+
+end
---
+++ tests/when_getting_points.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require_relative "../tile_bag.rb"
+
+# Test class for the Word's score method
+class WhenGettingPoints < Minitest::Test
+
+
+end
