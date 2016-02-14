04b8ca4d0247a300c722288fd366e0fd103b5e3c
Matt Maginniss <maginniss.matt@gmail.com>
2016-02-14 16:14:34 -0500

    Wrote all test for the TileRack#has_tiles_for? method. They all work properly.

--- tests/when_seeing_if_a_tile_rack_has_the_tiles_needed_to_make_words.rb
+++ tests/when_seeing_if_a_tile_rack_has_the_tiles_needed_to_make_words.rb
@@ -9,4 +9,72 @@ class WhenSeeingIfATileRackHasTheTilesNeededToMakeWords   < Minitest::Test
 	@tr = TileRack.new
   end
 
+  def test_rack_has_needed_letters_when_letters_are_in_order_no_duplicates
+	@tr.append(:B)
+	@tr.append(:A)
+	@tr.append(:C)
+	@tr.append(:K)
+	
+	assert_equal(true, @tr.has_tiles_for?("BACK"))
+  end
+
+  def test_rack_has_needed_letters_when_letters_are_not_in_order_no_duplicates
+	@tr.append(:A)
+	@tr.append(:K)
+	@tr.append(:C)
+	@tr.append(:B)
+	
+	assert_equal(true, @tr.has_tiles_for?("BACK"))
+
+  end
+
+  def test_rack_doesnt_contain_any_needed_letters
+	@tr.append(:R)
+	@tr.append(:D)
+	@tr.append(:Z)
+	@tr.append(:P)
+	
+	assert_equal(false, @tr.has_tiles_for?("BACK"))
+
+  end
+
+  def test_rack_contains_some_but_not_all_needed_letters
+	@tr.append(:R)
+	@tr.append(:C)
+	@tr.append(:Z)
+	@tr.append(:P)
+	@tr.append(:A)
+	@tr.append(:Z)
+	@tr.append(:B)
+	
+	assert_equal(false, @tr.has_tiles_for?("BACK"))
+
+  end
+
+  def test_rack_contains_a_word_with_duplicate_letters
+	@tr.append(:Z)
+	@tr.append(:O)
+	@tr.append(:O)
+	@tr.append(:P)
+	@tr.append(:T)
+	@tr.append(:H)
+	@tr.append(:T)
+	
+	assert_equal(true, @tr.has_tiles_for?("TOOTH"))
+
+
+  end
+
+  def test_rack_doesnt_contain_enough_duplicate_letters
+	@tr.append(:Z)
+	@tr.append(:P)
+	@tr.append(:O)
+	@tr.append(:P)
+	@tr.append(:U)
+	@tr.append(:H)
+	@tr.append(:T)
+	
+	assert_equal(false, @tr.has_tiles_for?("TOOTH"))
+  end
+  
 end
