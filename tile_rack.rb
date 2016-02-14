8c802b96a72756ef3a5aa9b8de59f951c2914296
Matt Maginniss <maginniss.matt@gmail.com>
2016-02-14 16:29:06 -0500

    Added comments and made sure the remove_word method returns the word

--- tile_rack.rb
+++ tile_rack.rb
@@ -6,10 +6,12 @@ class TileRack < TileGroup
 	  super
   end
 
+  # This method will return the number of tiles needed to be drawn to have a full tile rack.
   def number_of_tiles_needed
     7 - @tiles.length
   end
   
+  # This method will take a word and check to see if there are enough tiles to properly make it.
   def has_tiles_for?(text)
 	tile_string = to_s
 	result = true
@@ -26,8 +28,12 @@ class TileRack < TileGroup
 	result
   end
   
+  # This method will remove and return the word from the tile array.
   def remove_word(text)
-    text.each_char { |x| remove(x.to_sym) }
+	word = ""
+    text.each_char { |x| word += remove(x.to_sym) }
+	
+	word
   end
   
 end
