c0f75e2d2ffc3056cf87b81b8e1b4b813e4935c1
Matt Maginniss <maginniss.matt@gmail.com>
2016-02-20 15:40:30 -0500

    Created basic run method that will currently just print out the input and the tiles on the rack.

--- babble.rb
+++ babble.rb
@@ -1,11 +1,35 @@
+require '../rabble/tile_group.rb'
+require '../rabble/tile_bag.rb'
+require '../rabble/tile_rack.rb'
+
 # Class that is the driver for the program.
 class Babble
+	
+	# This will initialize the babble driver class.
 	def initialize()
 	end
 	
+	# The main program loop.
 	def run()
-	
+		tb = TileBag.new
+		tg = TileGroup.new
+		rack = TileRack.new
+		input = ""
+		
+		until input == ":quit\n" do
+			rack.number_of_tiles_needed.times do |x|
+				rack.append(tb.draw_tile)
+			end
+			
+			puts rack.to_s
+			
+			input = gets
+			
+			puts "Output:", input
+		end
 	end
 
 
 end
+
+Babble.new.run
